4f8e2b4b3befeed646f1135cb0712c73f69f683a
Maronato <gustavomaronato@gmail.com>
2020-12-16 02:22:14 -0300

    include Expires header on JWT list view

--- rest_framework_simplejwt/jwk/views.py
+++ rest_framework_simplejwt/jwk/views.py
@@ -1,6 +1,8 @@
 from django.db.models.query import QuerySet
+from django.utils.http import http_date
 from rest_framework import permissions, views, request, response, renderers
 from .models import JWK
+from ..utils import datetime_to_epoch
 
 
 class JWKList(views.APIView):
@@ -8,6 +10,11 @@ class JWKList(views.APIView):
     renderer_classes = [renderers.JSONRenderer]
 
     def get(self, request: request.Request):
+        current_jwk = JWK.get_current_jwk()
         jwks: QuerySet[JWK] = JWK.objects.all()
         keys = list(map(lambda k: k.to_jwk(), jwks))
-        return response.Response({"keys": keys}, content_type="application/json")
+        return response.Response(
+            {"keys": keys},
+            content_type="application/json",
+            headers={"Expires": http_date(datetime_to_epoch(current_jwk.expires_at))},
+        )
