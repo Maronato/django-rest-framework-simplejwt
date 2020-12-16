1c1e9738f50a9450e82c1111ea4f61e1e1a7254f
Maronato <gustavomaronato@gmail.com>
2020-12-16 02:08:07 -0300

    only render JWK list using json

--- rest_framework_simplejwt/jwk/views.py
+++ rest_framework_simplejwt/jwk/views.py
@@ -1,10 +1,11 @@
 from django.db.models.query import QuerySet
-from rest_framework import permissions, views, request, response
+from rest_framework import permissions, views, request, response, renderers
 from .models import JWK
 
 
 class JWKList(views.APIView):
     permission_classes = [permissions.AllowAny]
+    renderer_classes = [renderers.JSONRenderer]
 
     def get(self, request: request.Request):
         jwks: QuerySet[JWK] = JWK.objects.all()
